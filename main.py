29590164f2f3158cbac55c035f71554043351cfa
MrPilotMan <33531798+MrPilotMan@users.noreply.github.com>
2018-07-20 18:51:56 -0400

    fix mistake

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ account = account_info(generate_passphrase())
 for k, v in account.items():
     print(k, "\n", v, "\n")
 
-    reed_solomon_qrcode = make_qr(account["reed solomon"], "reed solomon")
-    passphrase_qrcode = make_qr(account["passphrase"], "passphrase")
+reed_solomon_qrcode = make_qr(account["reed solomon"], "reed solomon")
+passphrase_qrcode = make_qr(account["passphrase"], "passphrase")
 
 
